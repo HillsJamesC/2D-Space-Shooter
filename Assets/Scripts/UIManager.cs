831e5b149eea647753b01e0b2d68c1e58bd14442
James Hills <16933361+HillsJamesC@users.noreply.github.com>
2022-05-22 15:49:23 -0500

    Created Lives Display

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,14 +7,23 @@ public class UIManager : MonoBehaviour
 {
     // Handle to Text
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Sprite[] _liveSprites;
+    [SerializeField] private Image _LivesImg;
+
     // Start is called before the first frame update
     void Start()
     {
+        
         _scoreText.text = "Score: " + 0;
     }
 
     public void UpdateScore(int playerScore)
     {
         _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives (int currentLives)
+    {
+        _LivesImg.sprite = _liveSprites[currentLives];
     }
 }
