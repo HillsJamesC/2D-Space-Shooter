e221bffb59d40f6d8b602dc98a064d8bb78a6ddd
James Hills <16933361+HillsJamesC@users.noreply.github.com>
2022-06-08 11:49:10 -0500

    Added Enemy Laser with 1 Damage/Shot

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField]
-    private float _speed = 4.0f;
+    [SerializeField] private float _speed = 4.0f;
     private Player _player;
     private Animator _anim;
     private Collider2D _enemyCollider;
     private AudioSource _audioSource;
+    [SerializeField] private GameObject _laserPrefab;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -39,19 +41,37 @@ public class Enemy : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        CalculateMovement();
+
+        if (Time.time > _canFire)
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+            
+            for (int i = 0; i < lasers.Length; i++)
+            {
+                lasers[i].AssignEnemyLaser();
+            }
+        }
+    }
+
+    void CalculateMovement()
+    {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
         if (_enemyCollider.enabled == false && transform.position.y < -4.92f)
         {
             Destroy(this.gameObject);
         }
-        
+
         if (transform.position.y < -4.92f)
         {
             float randomX = Random.Range(-9f, 9f);
             transform.position = new Vector3(randomX, 7.12f, 0);
-        }        
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
