1d47b66fc94fa50fe53fd6ac1158bc364bb71922
James Hills <16933361+HillsJamesC@users.noreply.github.com>
2022-07-12 16:22:27 -0500

    Corrected SpawnManager for New Slow Speed Powerup

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -95,7 +95,7 @@ public class SpawnManager : MonoBehaviour
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-9f, 9f), 7.12f, 0);
-            int randomPowerUp = Random.Range(0, 5);
+            int randomPowerUp = Random.Range(0, 6);
             Instantiate(_powerups[randomPowerUp], posToSpawn, Quaternion.identity);
             yield return new WaitForSeconds(Random.Range(3, 7));
         }
